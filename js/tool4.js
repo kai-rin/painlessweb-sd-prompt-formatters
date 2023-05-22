d916772dcc7ad55fe9fcf17a1b1b73fb9ef2ba2e
Kairin <127017108+kai-rin@users.noreply.github.com>
2023-05-22 14:22:01 +0900

    Change Tool 1 to newlines2commas, and Tool 4 bug fix

--- js/tool4.js
+++ js/tool4.js
@@ -1,13 +1,22 @@
 document.getElementById("tool4-button").addEventListener("click", function() {
-    const input = document.getElementById("tool4-input").value;
-    let output = input;
+    const inputText = document.getElementById("tool4-input").value;
 
-    // Removing all parentheses and anything between colon and parentheses.
-    let previous;
-    do {
-        previous = output;
-        output = output.replace(/\(([^:)]*):[^)]*\)/g, '$1');
-    } while (previous !== output);
+    // Temporarily replace the <> sections to not be affected by the following replaces
+    const placeholders = [];
+    const tempInput = inputText.replace(/\(.*?\)/g, match => {
+        placeholders.push(match);
+        return `<${placeholders.length - 1}>`;
+    });
 
-    document.getElementById("tool4-output").value = output;
+    let outputText = tempInput
+        .replace(/:\d+(\.\d+)?/g, '')
+        .replace(/[\(\)]/g, '')
+        .trim();
+      
+    // Replace back the placeholders to their original form
+    placeholders.forEach((placeholder, i) => {
+        outputText = outputText.replace(new RegExp(`<${i}>`, 'g'), placeholder.replace(/[\(\):.\d]/g, ''));
+    });
+
+    document.getElementById("tool4-output").value = outputText;
 });
