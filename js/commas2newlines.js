9facef82ec52a82e36d66956a98b09878c795c64
Kairin <127017108+kai-rin@users.noreply.github.com>
2023-05-22 15:14:16 +0900

    Chaged from Tool 2 to commas2newlines

---
+++ js/commas2newlines.js
@@ -0,0 +1,5 @@
+document.getElementById("commas2newlines-button").addEventListener("click", function(){
+    const input = document.getElementById("commas2newlines-input").value;
+    const output = input.split(/\s*,\s*/).join('\n');
+    document.getElementById("commas2newlines-output").value = output;
+});
--- js/tool2.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("tool2-button").addEventListener("click", function(){
-    const input = document.getElementById("tool2-input").value;
-    const output = input.split(/\s*,\s*/).join('\n');
-    document.getElementById("tool2-output").value = output;
-});
